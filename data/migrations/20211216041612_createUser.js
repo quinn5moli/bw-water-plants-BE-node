20c0d88303073909ecfba35a9417789f8260106d
quinn5moli <quinn.moli@gmail.com>
2021-12-16 19:25:32 -0800

    createUser schema populated

--- data/migrations/20211216041612_createUser.js
+++ data/migrations/20211216041612_createUser.js
@@ -1,8 +1,14 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('username').notNullable().unique();
+      table.string('phoneNumber').notNullable();
+      table.string('password').notNullable();
+
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('users');
 };
